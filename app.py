65116d668275a3b3e5ed58982ba9ff9c346c9e35
Kyle Heckman <heckman.kyle16@gmail.com>
2020-04-21 20:03:51 -0400

    Fix encoding issue

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def authentication():
     }
 
     req = Request(url, headers=headers, method='POST')
-    response = urlopen(req, json.dumps(data))
+    response = urlopen(req, json.dumps(urlencode(data)))
     print(response.get_json())
 
     return "OK", 200
